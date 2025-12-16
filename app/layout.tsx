4d0399a75a2b149f0a63fe9638f513b33148bfd2
iqbal <ifachry@rocketmail.com>
2025-12-16 09:58:47 +0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "IndoProperty",
   description: "Indonesia Property Tokenization",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
